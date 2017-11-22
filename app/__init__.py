35ffc7ec6755a5f0764bc24fb9240bf7a4e94f1e
mirr254 <sammysteppa90@gmail.com>
2017-11-22 11:02:16 +0300

    [FEATURE] configures project directory

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+#!flask/bin/python
+from flask import Flask, jsonify,abort,request
+from flask import make_response
+
+def createApp():
+
+    app = Flask(__name__)
+    app.secret_key = 'jhjghjsdvvhgggjhsdvvvvhgsd'
+
+    users = []
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        return make_response( jsonify({'error': 'Resource not found'}), 404)
+
+    """ HANDLE USER ACTIVITIES"""
+
+    # register user
+    @app.route('/api/v1/auth/register', methods=['POST'])
+    def register():
+        if not request.json or not 'username' or not 'email' or not 'password' in request.json: #email must be included
+            abort(404)
+        user = {
+            'id':users[-1]['id'] + 1,
+            'email': request.json['email'],
+            'username':request.json['username'],
+            'password':request.json['password']
+        }
+        users.append(user)
+        return jsonify({'task': user}),201
+
+
+    """ HANDLE EVENTS ACTIVITIES """
+
+    return app
