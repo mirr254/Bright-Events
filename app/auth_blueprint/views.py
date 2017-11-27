537a927b9f7a84cd26bed90ce6edc2f476af7caa
mirr254 <sammysteppa90@gmail.com>
2017-11-27 15:47:35 +0300

    [FEATURE] Creates a development branch

--- app/auth_blueprint/views.py
+++ app/auth_blueprint/views.py
@@ -1,11 +1,27 @@
 #!flask/bin/python
 from flask import Flask, jsonify,abort,request
-from flask import make_response
+from flask import make_response, g
 from . import models
+from flask_httpauth import HTTPBasicAuth
 
 from . import auth
 
 """ HANDLE USER ACTIVITIES"""
+
+#http python authentication
+user_status = HTTPBasicAuth()
+
+#login
+@user_status.verify_password
+def login(email, password):
+    user = [user for user in models.User.users_list if user['email']== email and user['password']==password]
+    if user:
+        #return jsonify({'user':user})
+        print(jsonify({'user':user}))
+        return True
+    #abort(404)
+    return False        
+
 
 # register user
 @auth.route('/api/v1/auth/register', methods=['POST'])
@@ -32,10 +48,11 @@ def login(email, password):
     abort(403)
 
 
-#edit and event
-@auth.route('/api/v1/auth/reset-password', methods=['PUT'])
-def editEvent(email):
-    user = [user for user in models.User.users_list if user['email'] == email]
-    if len(user) == 0  and not request.json:
-        abort(404)
-    user[0]['email'] = request.json.get('email', user[0]['email'])
+#edit and password
+
+# @auth.route('/api/v1/auth/reset-password/<string:email>', methods=['PUT'])
+# def resetPassword(password):
+#     user = [user for user in models.User.users_list if user['email'] == 'him@']
+#     if len(user) == 0  and not request.json:
+#         abort(404)
+#     user[0]['password'] = request.json.get('password', user[0]['password'])
