19f92d2a0d5923bda09055acc10a957e5ddec0db
mirr254 <sammysteppa90@gmail.com>
2017-12-02 19:02:43 +0300

    [FIX] Fixes rendering error for api documentation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
-from app import createApp
+from app import createApp 
 
 app = createApp('development')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
