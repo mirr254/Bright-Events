7254cd5119f417901381088d39cafb4107911553
mirr254 <sammysteppa90@gmail.com>
2017-12-03 11:57:22 +0300

    [CHORE] Sets up and configures database enviroment for the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 #!flask/bin/python
 from flask import Flask
 from instance.config import app_config
+from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def createApp(conf_name):
 
-    app = Flask(__name__)    
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)   
 
       #register the blueprints 
     
--- instance/config.py
+++ instance/config.py
@@ -3,7 +3,9 @@ import os
 class Config(object):
     #parent configuration class
     DEBUG = False
-    SECRET = os.getenv('SECRETKEY')
+    CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET = os.getenv('SECRET')
 
 class DevelopmentConfig(Config):
     #configuration for development
@@ -12,8 +14,15 @@ class DevelopmentConfig(Config):
 class ProductionCofig(Config):
     DEBUG = False
 
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db_brighter_events'
+    DEBUG = True
+
 #export the enviroment specified
 app_config = {
     'development': DevelopmentConfig,
     'production': ProductionCofig,
+    'testing': TestingConfig
 }
