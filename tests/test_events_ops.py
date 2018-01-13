69d3fce7ac011a6d1e26107b9c5d94c95fc06810
mirr254 <sammysteppa90@gmail.com>
2018-01-14 00:06:17 +0300

    [FIX] Fixes all failing tests

--- tests/test_events_ops.py
+++ tests/test_events_ops.py
@@ -100,42 +100,59 @@ class EventsActivitiesTestCases(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
     
     def test_add_event_has_location(self):
-        res = self.client().post('/api/v1/events', data=json.dumps(self.event2), content_type='application/json')
+        token = self.get_verfication_token()
+
+        res = self.client().post('/api/v1/events',
+              headers = {'x-access-token' : token },
+              data=json.dumps(self.event2), content_type='application/json')
         self.assertEqual(res.status_code, 403)
 
     def test_add_event_has_name(self):
-        res = self.client().post('/api/v1/events', data=json.dumps(self.event3), content_type='application/json')
-        self.assertEqual(res.status_code, 403)
+        token = self.get_verfication_token()
 
-    def test_add_event_has_userid(self):
-        res = self.client().post('/api/v1/events', data=json.dumps(self.event4), content_type='application/json')
+        res = self.client().post('/api/v1/events', 
+            headers = {'x-access-token' : token },
+            data=json.dumps(self.event3), content_type='application/json')
         self.assertEqual(res.status_code, 403)
 
     #test api can get all events
     def test_can_get_events(self):
-        res = self.client().get('/api/v1/events', content_type='application/json')
+        token = self.get_verfication_token()
+
+        res = self.client().get('/api/v1/events',
+             headers = {'x-access-token' : token },
+             content_type='application/json')
         self.assertEqual(res.status_code, 200)
 
     #test rsvp
     def test_rsvp_an_event(self):
+        token = self.get_verfication_token()
         #make sure before rsvp we have an add event api working
-        resp = self.client().post('/api/v1/events', data=json.dumps(self.event1), content_type='application/json')
+        resp = self.client().post('/api/v1/events',
+             headers = {'x-access-token' : token },
+             data=json.dumps(self.event1), content_type='application/json')
         self.assertEqual(resp.status_code, 201)        
         
         res = self.client().post('/api/v1/events/1/rsvp',
+             headers = {'x-access-token' : token },
              data=json.dumps(self.rsvp_) ,content_type='application/json')
         self.assertEqual(res.status_code, 201)
 
     #test listing users who have responded (rsvp) to an event
     def test_list_rsvp_users(self):
+        token = self.get_verfication_token()
         #test create an event
-        resp = self.client().post('/api/v1/events', data=json.dumps(self.event1), content_type='application/json')
+        resp = self.client().post('/api/v1/events',
+            headers = {'x-access-token' : token }, 
+            data=json.dumps(self.event1), content_type='application/json')
         self.assertEqual(resp.status_code, 201)
         ### test user can rsvp to that event ###
         result_in_json = json.loads(resp.data.decode('utf-8').replace("'", "\""))
         #get eventid of newly created event        
-        res = self.client().post('/api/v1/events/1/rsvp',data=json.dumps(self.rsvp_) ,content_type='application/json')
-        print(res)
+        res = self.client().post('/api/v1/events/1/rsvp',
+            headers = {'x-access-token' : token },
+            data=json.dumps(self.rsvp_) ,content_type='application/json')
+        
         self.assertEqual(res.status_code, 201)
 
         #test the endpoint for retrieving the users. will retrieve users based on eventid
