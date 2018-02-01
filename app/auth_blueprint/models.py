ab75ea97de34119a244e020d21100efc799f46b8
mirr254 <sammysteppa90@gmail.com>
2018-02-01 22:41:16 +0300

    [CHORE] Creates a model for blacklisting tokens

--- app/auth_blueprint/models.py
+++ app/auth_blueprint/models.py
@@ -26,4 +26,21 @@ class User(db.Model):
         return "<User: {}>".format(self.username) #object instance of the model whenever it is queried
 
 
+class TokenBlackList(db.Model):
+    """ This model class wil hold blacklisted token for loggged out users"""
+
+    __tablename__ = 'blacklisted_tokens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(100), unique=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Token: {}>'.format(self.token)
+
+
 
