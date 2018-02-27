3229b363f1835b818628de734b0bfb0548251658
mirr254 <sammysteppa90@gmail.com>
2018-02-27 13:38:22 +0300

    [TESTS] Adds test for email confirmation token

--- tests/test_app_configuration.py
+++ tests/test_app_configuration.py
@@ -1,6 +1,8 @@
 import unittest
 from app import createApp
-from . import test_user_auth
+from app.auth_blueprint.views import generate_email_confirmation_token, confirm_email_confirmation_token
+#from app.utils.token import generate_email_confirmation_token,generate_password_reset_token,confirm_email_confirmation_token,confirm_password__reset_token
+ 
 
 class AppConfigTestCases(unittest.TestCase):
 
@@ -10,3 +12,15 @@ class AppConfigTestCases(unittest.TestCase):
     #test for debugging log
     def test_debug_mode(self):
         self.assertEqual(self.app.debug, False)
+
+class AppCommonFunctionsTestCases(unittest.TestCase):
+    def setUp(self):
+        self.app = createApp(conf_name='testing')
+        self.email = 'kungus@ymail.com'
+        self.email_confirmation_token = generate_email_confirmation_token(self.email)
+
+    #test for valid email confirmation token
+    def test_valid_email_conf_token(self):
+        email = confirm_email_confirmation_token(self.email_confirmation_token,3600)
+        self.assertEqual(email, self.email)
+    
