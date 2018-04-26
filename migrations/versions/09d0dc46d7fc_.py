6d69003755857f79d975c41be74c218cdd0ba035
samuel ngigi <shammir@Tonie-MacBook-Pro.local>
2018-04-26 13:09:46 +0300

    [Feature ]implements LF feedback on rsvp and events

--- migrations/versions/09d0dc46d7fc_.py
+++ migrations/versions/09d0dc46d7fc_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: e628fcc45c75
+Revision ID: 09d0dc46d7fc
 Revises: 
-Create Date: 2018-03-21 01:32:30.199463
+Create Date: 2018-04-26 11:49:18.646484
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e628fcc45c75'
+revision = '09d0dc46d7fc'
 down_revision = None
 branch_labels = None
 depends_on = None
