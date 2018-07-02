bd0c4fb73de05837fc062e9828e6ccbc0b3234a4
mirr254 <sammysteppa90@gmail.com>
2018-07-02 21:39:19 +0300

    [Chore] AWS rds configuration for production db instance

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config(object):
+    #aws rds configuration
+    driver = 'postgresql+psycopg2://'
+
     #parent configuration class
     DEBUG = False
     CSRF_ENABLED = True
@@ -29,6 +32,10 @@ class DevelopmentConfig(Config):
 
 class ProductionCofig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = driver \
+                                + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] \
+                                +'@' + os.environ['RDS_HOSTNAME']  +  ':' + os.environ['RDS_PORT'] \
+                                + '/' + os.environ['RDS_DB_NAME']
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
