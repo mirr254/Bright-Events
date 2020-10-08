89c36bd1633761312207cde374b60534952d0613
mirr254 <sammysteppa90@gmail.com>
2020-10-08 13:21:00 +0300

    remove docker layer caching

--- resources/dependencies.sh
+++ resources/dependencies.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-CIRCLE_CACHE_DIR="/usr/loca/bin"
+CIRCLE_CACHE_DIR="/usr/local/bin"
 PACKER_VERSION="1.6.2"
 PACKER_URL="https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
-TERRAFORM_VERSION="0.12.10"
-TERRAFORM_URL= "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+TERRAFORM_VERSION="0.12.13"
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 
-if [ ! -f "${CIRCLE_CACHE_DIR/packer" ] || [[ ! "$(packer version)" =~ "Packer v${PACKER_VERSION}" ]]; then
+if [ ! -f "${CIRCLE_CACHE_DIR}/packer" ] || [[ ! "$(packer version)" =~ "Packer v${PACKER_VERSION}" ]]; then
     wget -O /tmp/packer.zip "${PACKER_URL}"
     unzip -oud "${CIRCLE_CACHE_DIR}" /tmp/packer.zip
 fi
@@ -17,6 +17,7 @@ if [ ! -f "${CIRCLE_CACHE_DIR}/terraform" ] || [[ "$(terraform version)" =~ "Ter
     wget -O /tmp/terraform.zip "${TERRAFORM_URL}"
     unzip -oud "${CIRCLE_CACHE_DIR}" /tmp/terraform.zip
 fi
+
 
 packer version
 terraform version
